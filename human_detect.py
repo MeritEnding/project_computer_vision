2506f5dadd1d94603537672b41d6d3625ef5c16b
leejunghoon <dksldsk@naver.com>
2023-10-11 21:47:21 +0900

    :memo: 최소 얼굴 크기 및 최대 허용 얼굴 수 개선 추가 master

--- human_detect.py
+++ human_detect.py
@@ -1,16 +1,21 @@
 import cv2
 import numpy as np
 
+# 전역 변수 설정
+MAX_ALLOWED_FACES = 10  # 허용되는 최대 얼굴 수
+MIN_FACE_SIZE = (30, 30)  # 감지할 얼굴의 최소 크기
+
 # OpenCV의 Haar Cascade Classifier를 사용하여 얼굴을 감지하는 함수
 def detect_faces(frame):
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=MIN_FACE_SIZE)
     return faces
 
 # 경고 함수
-def send_warning():
-    print("인구가 너무 많습니다! 경고를 보냅니다.")
+def send_warning(num_faces):
+    if num_faces > MAX_ALLOWED_FACES:
+        print(f"인구가 너무 많습니다! 얼굴 수: {num_faces}")
 
 # 메인 함수
 def main():
@@ -28,8 +33,7 @@ def main():
         num_faces = len(faces)
 
         # 인구 수에 따라 경고 보내기
-        if num_faces > 10:
-            send_warning()
+        send_warning(num_faces)
 
         # 화면에 얼굴 표시
         for (x, y, w, h) in faces:
